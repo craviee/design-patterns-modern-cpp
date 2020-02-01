34b82897b5ed17500020a379f03e92ba7a70961b
craviee <danielcraviee@gmail.com>
2020-02-01 13:12:34 +0100

    Iterator Design Pattern
    The aim of the iterator design pattern is to visit elements sequentially in a complex structure like default arrays.
    It is possible to use iterators to implement a order of visits in trees, for example.
    This example uses the iterator from std::vector, which can visit all elements on a vector from the first position to the last,
    and also in a reverse order.

--- Behavioral/Iterator/src/main.cpp
+++ Behavioral/Iterator/src/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
+    std::vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    for(auto iterator = numbers.begin(); iterator != numbers.end(); iterator++)
+        std::cout << "This number is: " << *iterator << std::endl;
+
+    for(auto iterator = numbers.begin(); iterator != numbers.end(); iterator++)
+    {
+        (*iterator)++;
+        std::cout << "Adding 1 to the number: " << *iterator << std::endl;
+    }
+
+    for(auto reverseIterator = numbers.rbegin(); reverseIterator != numbers.rend(); reverseIterator++)
+        std::cout << "Reverse order: " << *reverseIterator << std::endl;
+        
     return 0;
 }
